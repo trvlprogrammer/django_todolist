656cbb52f7aca4b2e9e015bee2e44db02b0a5c1b
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-10-14 10:17:58 +0700

    update readme and requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# todolist
- Todo list app using django
+Todo list app to learn basic django
+
+Feature on this app:
+1. Register User
+2. Login User
+3. CRUD 
+4. Export CSV
+5. Import CSV
+6. Export and email CSV
+7. Logging
+8. Handler for 404 and 500
+
+
+
+How to setup:
+1. Make sure you have python 3 and install virtual environment library
+2. Create virtual environtment "python3 -m virtualenv env"
+3. Activate virtual env "env\Scripts\activate"(windows) or "source env\bin\activate"(unix)
+4. Install python dependencies cd to this repo folder "pip install -r requirements.txt"
+5. Create configuration file, copy file.env and rename to .env and change all the values.
+6. Running the apps "python manage.py runserver"
--- todolist/settings.py
+++ todolist/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-EXPORT_PATH = os.environ.get('EXPORT_PATH')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     "whitenoise.middleware.WhiteNoiseMiddleware", #whitenoise to load static file when debug = false
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,7 +65,6 @@ LOGIN_REDIRECT_URL="todos:index"
 LOGOUT_REDIRECT_URL='accounts:login'
 
 # email configuration
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_USE_TLS = eval(os.environ.get('EMAIL_USE_TLS'))
@@ -88,6 +88,7 @@ TEMPLATES = [
     },
 ]
 
+# logging configuration
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -160,7 +161,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
